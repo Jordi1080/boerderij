c4c58827ef5e34d2c46eefb1c1a4aab9ddd855e9
EJoling <emieljoling@gmail.com>
2019-03-01 16:47:59 +0100

    Methode schoonmaken(fles) aangepast

--- Schoonmaker.java
+++ Schoonmaker.java
@@ -9,18 +9,18 @@ public class Schoonmaker {
 		this.naam = naam;
 	}
 	
-	void schoonmaken() {
+	void schoonmaken(Dier dier) {
 		 schoneHanden = false;
-		 System.out.println("Dat is lekker schoon, maar mijn handen zijn vies");
+		 System.out.println("Zo, "+dier.naam+" is schoon, maar mijn handen zijn vies");
 	 }//end schoonmaken
 	
 	void schoonmaken(Fles fles) {
 		if(fles.inhoud > 0) {
-			System.out.println("Ah lekker, er is zit nog melk in. Jam Jam");
+			System.out.println("Ah lekker, er is zit nog" +fles.soortInhoud+" in. Jam Jam");
 			fles.inhoud = 0;
 		}
 		else{
-			System.out.println("Zo, ff lekker deze " +fles + "schoonmaken.");
+			System.out.println("Zo, ff lekker deze " +fles.soortInhoud + "fles schoonmaken.");
 		}
 		 schoneHanden = false;
 		 System.out.println("Dat is een schone" + fles);
