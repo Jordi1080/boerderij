d849d4b6c5abfa88f75638003a4e9b02a8025527
Jordi1080 <jordi.hesselz@hotmail.com>
2019-03-01 15:11:06 +0100

    derde commit of zo

--- Boerderij.java
+++ Boerderij.java
@@ -1,5 +1,18 @@
 package boerderij;
+import java.util.ArrayList;
 
 public class Boerderij {
-
+	ArrayList<Dier> dieren;// = new ArrayList<>();
+	ArrayList<Fles> flessen;// = new ArrayList<>();
+	Boer boer;
+	int geld;
+	
+	public Boerderij(){
+		 dieren = new ArrayList<>();
+		 flessen = new ArrayList<>();
+		 boer = new Boer();
+		 geld = 1000;
+	}
+	
+	
 }
