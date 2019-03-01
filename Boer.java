dac5fb908b81e1f82b7a7fd3ca57a75f8838d037
NickWaal <nick_waal@hotmail.com>
2019-03-01 15:06:07 +0100

    Boer aangemaakt

---
+++ Boer.java
@@ -0,0 +1,26 @@
+package boerderij;
+// dier melken
+// 
+//
+public class Boer {
+	String naam;
+	int leeftijd;
+
+	Boer(String naam, int leeftijd) {
+		this.naam = naam;
+		this.leeftijd = leeftijd;
+	}
+	
+	void dierMelken(Dier dier) {
+		System.out.println("Ik ga het dier melken");
+	}
+
+	
+	
+}
+
+
+
+
+
+ class Dier{}
