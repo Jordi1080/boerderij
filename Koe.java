116a039bd92bad76c4effde044d0a4e6b42c85c2
LoesPK <loeskockelmans@gmail.com>
2019-03-01 15:46:55 +0100

    laatste voor pullrequest

--- Koe.java
+++ Koe.java
@@ -2,7 +2,11 @@ package boerderij;
 
 public class Koe extends Dier{
 	int hoeveelheidMelk = 200;
+	String naam;
 	void geluidMaken() {
 			System.out.println("Moeeeeeeeee");
 		}
+	Koe(String naam){
+		this.naam = naam;
+	}
 }
