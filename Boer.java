6abc5cf113af38cbbaf8643e1f26d83bca766d6f
NickWaal <nick_waal@hotmail.com>
2019-03-01 15:14:10 +0100

    Tweede commit

--- Boer.java
+++ Boer.java
@@ -1,4 +1,5 @@
 package boerderij;
+
 // dier melken
 // 
 //
@@ -10,17 +11,13 @@ public class Boer {
 		this.naam = naam;
 		this.leeftijd = leeftijd;
 	}
-	
+
 	void dierMelken(Dier dier) {
 		System.out.println("Ik ga het dier melken");
 	}
 
-	
-	
+	void flesPakken(Fles fles) {
+		System.out.println("Ik pak de fles");
+	}
+
 }
-
-
-
-
-
- class Dier{}
